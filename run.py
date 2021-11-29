09385887df445f0906e137cb545aca1793e30fd4
Mario Garcia <mariogarcia.ar@gmail.com>
2021-11-29 20:27:47 -0300

    Agrego la exportacion a CSV y tambien la estructura de JSON

--- run.py
+++ run.py
@@ -11,7 +11,10 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support import expected_conditions as EC
 
 import time, os
+from datetime import datetime
+from datetime import timedelta
 import numpy as np
+import pandas as pd
 from shutil import copyfile
 
 # -----------------------------------------------------------------------------
@@ -20,10 +23,10 @@ fx.init()
 
 
 dir_path_ute_download = fx.dir_download+'data_nivel_aporte/'
-dir_path_ute_data = fx.dir_data+'data_nivel_aporte/'
+dir_path_ute_csv = fx.dir_data+'data_nivel_aporte/'
 
 os.makedirs(dir_path_ute_download, exist_ok = True) 
-os.makedirs(dir_path_ute_data, exist_ok = True) 
+os.makedirs(dir_path_ute_csv, exist_ok = True) 
 
 
 url = "https://apps.ute.com.uy/SgePublico/BajadasGE.aspx"
@@ -71,28 +74,50 @@ def process(row):
                     fx.drowpdown_select_byvalue(el_id="ctl00_ContentPlaceHolder1_cboPasos", option_value=paso_id, driver=driver)
 
                     # print("\nEjecutando",paso, estacion, subcuenca, cuenca)
-                    print("\nEjecutando", cuenca)
-                    dst = dir_path_ute_data+"/{}-{}-{}-{}-{}-{}-{}-{}.txt".format(
+
+                    dst_csv = dir_path_ute_csv+"/{}-{}-{}-{}-{}-{}-{}-{}.txt".format(
                         row["cboAnioIni"],row["cboMesIni"],row["cboAnioFin"],row["cboMesFin"],
                         cuenca_id, subcuenca_id, estacion_id, paso_id
                     )
-                    if os.path.exists(dst):
-                        print('Proccesed', dst)
+                    dst_csv = dst_csv.replace('.txt','.csv')
+                    if os.path.exists(dst_csv):
+                        print('Proccesed', dst_csv)
                         continue
 
 
-                    print('new', dst)
+                    print('new', dst_csv)
                     fx.download_from_driver(driver)
                     src = fx.showLastFileCreated(dir_path_ute_download)
-                    copyfile(src, dst)
+                    # copyfile(src, dst)
+                   
+                    exportRawToCsv(src, dst_csv)
             
-    fx.exportJsonToFile(fx.dir_data+'ute.json', cuencas)      
+    fx.exportJsonToFile(fx.dir_data+"/{}-{}-{}-{}-ute.json".format(
+                        row["cboAnioIni"],row["cboMesIni"],row["cboAnioFin"],row["cboMesFin"]
+                    ), cuencas)      
 
 
+dateparse = lambda x: datetime.strptime(x, '%d/%m/%Y') # %Y-%m-%d %H:%M:%S
+col_names = ['date','hour','cuenca','subcuenca','x1','estacion','nivel','x2','x3']
+
+def exportRawToCsv(file_src, file_to):
+    df = pd.read_csv(file_src, encoding='ISO-8859-1', 
+                names=col_names,sep=";",skiprows=2,
+                parse_dates=["date"],date_parser=dateparse)
+    df['dt'] = df['date'].astype(str) +' '+ df['hour'].apply(str).str[:-2] +':00:00' # pd.DateOffset(hours=df['hour']/100) #timedelta(2)
+    df['dt'] = pd.to_datetime(df['dt'])
+
+    df_obj = df.select_dtypes(['object'])
+    df[df_obj.columns] = df_obj.apply(lambda x: x.str.strip())
+
+    df.drop(['date','hour' ], inplace=True, axis=1)   
+    df.to_csv(file_to, index = False) 
 
 # -----------------------------------------------------------------------------
-months = ["Enero","Junio","Diciembre"]
+# months = ["Enero","Junio","Diciembre"]
 # years = [*range(2000,2021)]
+
+months = ["Enero","Junio"]
 years = [*range(2005,2006)]
 
 for iy, y in enumerate(years):
